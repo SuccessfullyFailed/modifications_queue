5790e58c9f6835f600150b948da2cf3412251c31
SuccessfullyFailed <renzogerritzen@gmail.com>
2026-04-03 18:44:39 +0200

    Make mutable queue the default and remove immutable queue

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
 mod modifications_queue;
 mod modifications_queue_u;
-mod modifications_queue_mut;
-mod modifications_queue_mut_u;
 
 pub use modifications_queue::*;
-pub use modifications_queue_mut::*;
--- src/modifications_queue.rs
+++ src/modifications_queue.rs
@@ -2,13 +2,14 @@ use std::{ time::Duration, sync::{ Arc, Condvar, Mutex, MutexGuard } };
 
 
 
-struct ModificationsQueueInner<T> {
-	data:Mutex<Vec<Box<dyn FnMut(&T) + Send + Sync + 'static>>>,
+
+struct ModificationsQueueInnerMut<T> {
+	data:Mutex<Vec<Box<dyn FnMut(&mut T) + Send + Sync + 'static>>>,
 	cond:Condvar
 }
-impl<T> Default for ModificationsQueueInner<T> {
+impl<T> Default for ModificationsQueueInnerMut<T> {
 	fn default() -> Self {
-		ModificationsQueueInner {
+		ModificationsQueueInnerMut {
 			data: Mutex::new(Vec::new()),
 			cond: Condvar::new()
 		}
@@ -17,33 +18,34 @@ impl<T> Default for ModificationsQueueInner<T> {
 
 
 
-pub struct ModificationsQueue<T>(Arc<ModificationsQueueInner<T>>);
-impl<T> ModificationsQueue<T> {
+pub struct ModificationsQueueMut<T>(Arc<ModificationsQueueInnerMut<T>>);
+impl<T> ModificationsQueueMut<T> {
 
 	/// Create a new queue.
-	pub fn new() -> ModificationsQueue<T> {
+	pub fn new() -> ModificationsQueueMut<T> {
 		Self::default()
 	}
 
 	/// Creates a remote to this queue.
 	/// Allows changes from multiple places without having to borrow the queue.
-	pub fn create_remote(&self) -> ModificationsQueueRemote<T> {
-		ModificationsQueueRemote(Arc::clone(&self.0))
+	pub fn create_remote(&self) -> ModificationsQueueRemoteMut<T> {
+		ModificationsQueueRemoteMut(Arc::clone(&self.0))
 	}
 
 	/// Add an item to the queue.
-	pub fn add<Modification:FnMut(&T) + Send + Sync + 'static>(&self, modification:Modification) {
+	pub fn add<Modification:FnMut(&mut T) + Send + Sync + 'static>(&self, modification:Modification) {
 		self.0.data.lock().unwrap().push(Box::new(modification));
 	}
 
 	/// Drain all modifications.
-	pub fn drain(&self) -> Vec<Box<dyn FnMut(&T) + Send + Sync + 'static>> {
+	pub fn drain(&self) -> Vec<Box<dyn FnMut(&mut T) + Send + Sync + 'static>> {
 		self.0.data.lock().unwrap().drain(..).collect()
 	}
 
 	/// Apply all modifications to the given struct.
 	pub fn apply_to(&self, target:&mut T) {
-		for mut modification in self.drain() {
+		let modifications:Vec<Box<dyn FnMut(&mut T) + Send + Sync>> = self.drain();
+		for mut modification in modifications {
 			modification(target);
 		}
 	}
@@ -51,8 +53,8 @@ impl<T> ModificationsQueue<T> {
 	/// Puts the thread to sleep until anything is added to the queue.
 	/// If something is already in the queue, it will immediately return that.
 	/// Drains and returns all data in the queue.
-	pub fn await_change(&self) -> Vec<Box<dyn FnMut(&T) + Send + Sync + 'static>> {
-		let mut data_handle:MutexGuard<'_, Vec<Box<dyn FnMut(&T) + Send + Sync + 'static>>> = self.0.data.lock().unwrap();
+	pub fn await_change(&self) -> Vec<Box<dyn FnMut(&mut T) + Send + Sync + 'static>> {
+		let mut data_handle:MutexGuard<'_, Vec<Box<dyn FnMut(&mut T) + Send + Sync + 'static>>> = self.0.data.lock().unwrap();
 		while data_handle.is_empty() {
 			data_handle = self.0.cond.wait(data_handle).unwrap();
 		}
@@ -62,32 +64,32 @@ impl<T> ModificationsQueue<T> {
 	/// Puts the thread to sleep until anything is added to the queue or the given duration has surpassed.
 	/// If something is already in the queue, it will immediately return that.
 	/// Drains and returns all data in the queue.
-	pub fn await_change_timeout(&self, timeout:Duration) -> Vec<Box<dyn FnMut(&T) + Send + Sync + 'static>> {
-		let mut data_handle:MutexGuard<'_, Vec<Box<dyn FnMut(&T) + Send + Sync + 'static>>> = self.0.data.lock().unwrap();
+	pub fn await_change_timeout(&self, timeout:Duration) -> Vec<Box<dyn FnMut(&mut T) + Send + Sync + 'static>> {
+		let mut data_handle:MutexGuard<'_, Vec<Box<dyn FnMut(&mut T) + Send + Sync + 'static>>> = self.0.data.lock().unwrap();
 		while data_handle.is_empty() {
 			data_handle = self.0.cond.wait_timeout(data_handle, timeout).unwrap().0;
 		}
 		data_handle.drain(..).collect()
 	}
 }
-impl<T> Default for ModificationsQueue<T> {
+impl<T> Default for ModificationsQueueMut<T> {
 	fn default() -> Self {
-		ModificationsQueue(Arc::new(ModificationsQueueInner::default()))
+		ModificationsQueueMut(Arc::new(ModificationsQueueInnerMut::default()))
 	}
 }
 
 
 
-pub struct ModificationsQueueRemote<T>(Arc<ModificationsQueueInner<T>>);
-impl<T> ModificationsQueueRemote<T> {
+pub struct ModificationsQueueRemoteMut<T>(Arc<ModificationsQueueInnerMut<T>>);
+impl<T> ModificationsQueueRemoteMut<T> {
 
 	/// Add an item to the queue this remote targets.
-	pub fn add<Modification:FnMut(&T) + Send + Sync + 'static>(&self, modification:Modification) {
+	pub fn add<Modification:FnMut(&mut T) + Send + Sync + 'static>(&self, modification:Modification) {
 		self.0.data.lock().unwrap().push(Box::new(modification));
 	}
 }
-impl<T> Clone for ModificationsQueueRemote<T> {
+impl<T> Clone for ModificationsQueueRemoteMut<T> {
 	fn clone(&self) -> Self {
-		ModificationsQueueRemote(Arc::clone(&self.0))
+		ModificationsQueueRemoteMut(Arc::clone(&self.0))
 	}
 }
